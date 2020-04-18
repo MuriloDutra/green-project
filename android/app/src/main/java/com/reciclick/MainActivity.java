db8a91813008b6c87c7b7d228fe83f5151e54360
Murilo <murilo.alves@watermelontecnologia.com>
2020-04-17 22:25:59 -0300

    Alterando nome e adicionando símbolo do aplicativo/gerando apk

--- android/app/src/main/java/com/reciclick/MainActivity.java
+++ android/app/src/main/java/com/reciclick/MainActivity.java
@@ -1,4 +1,4 @@
-package com.projectgreen;
+package com.reciclick;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "projectGreen";
+    return "reciclick";
   }
 }
--- android/app/src/main/java/com/reciclick/MainApplication.java
+++ android/app/src/main/java/com/reciclick/MainApplication.java
@@ -1,4 +1,4 @@
-package com.projectgreen;
+package com.reciclick;
 
 import android.app.Application;
 import android.content.Context;
@@ -64,7 +64,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.projectgreen.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.reciclick.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
